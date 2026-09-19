7b0f9d2ddc4d468a348a82033eeffed31c5f87a8
medlaksai <laksai018@gmail.com>
2026-09-19 10:20:49 +0100

    Upd

--- Loops/Challenge3.js
+++ Loops/Challenge3.js
@@ -2,9 +2,10 @@ const prompt = require("prompt-sync")();
 
 let n = parseInt(prompt("Enter an integer :"));
 
+let sum = 0;
 
-for (let i = 1 ; i <= n ; i++ ){
-     let sum = i + n; 
+for (let i = 1 ; i <= 10 ; i++ ){
+     sum = i; 
      console.log(sum)
 }
     
--- Loops/Challenge4.js
+++ Loops/Challenge4.js
@@ -1,9 +1,11 @@
 const prompt = require("prompt-sync")();
 
-let n = parseInt(prompt("Enter an integer  : "))
+let n = parseInt(prompt("Enter an integer :"));
+
 let sum = 0;
 
-for (let i = 1 ; i < n ; i++){
-    sum = ++i;
-     console.log(n)
+for (let i = 0 ; i <= 10 ; i++ ){
+     sum += i; 
+     console.log(sum)
 }
+    
